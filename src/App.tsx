d653ded81ec623ee52d1318d6e246e6c8e5df09b
ramonnezzz <savioramonnogueira@gmail.com>
2025-09-07 16:10:51 -0300

    feat: implement main application logic and header component for to-do list

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import './App.css'
+
+export interface Tarefa {
+  id: number;
+  tarefa: string;
+  selecionado: boolean;
+  completado?: boolean
+}
+
+function App() {
+  const [tarefa, setTarefas] = useState<Tarefa[]>([]);
+  const [inputTarefa, setInputTarefa] = useState("");
+  function addTarefa(){
+    if(inputTarefa.trim() === '')return;
+
+    const novaTarefa = {
+      id: Math.random(),
+      tarefa: inputTarefa,
+      selecionado: false,
+
+    };
+    setTarefas([...tarefa, novaTarefa]); setInputTarefa('');
+  }
+
+  function selecionarTarefa(tarefaSelecionada: Tarefa){
+    const tarefasAtualizadas = tarefa.map((tarefa) => ({...tarefa, selecionado: tarefa.id === tarefaSelecionada.id ? !tarefa.selecionado : false}));
+    setTarefas(tarefasAtualizadas);
+  }
+
+  function completarTarefa(){
+    const tarefaSelecionada = tarefa.find((tarefa) => tarefa.selecionado);
+    if(!tarefaSelecionada) return;
+
+    const tarefasAtualizadas = tarefa.map((tarefa) => {
+      if(tarefa.id === tarefaSelecionada.id){
+        return {...tarefa, selecionado: false, completado: true};
+      }
+      return tarefa;
+    });
+    setTarefas(tarefasAtualizadas);
+  }
+
+  function limparTarefasCompletadas(){
+    const tarefasNaoCompletadas = tarefa.filter((tarefa) => !tarefa.completado);
+    setTarefas(tarefasNaoCompletadas);
+  }
+
+
+
+
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/header.tsx
@@ -0,0 +1,9 @@
+import './header.css'
+
+export function Header(){
+  return(
+    <header className="header">
+      <h1>to<span>do</span></h1>
+    </header>
+  )
+}
